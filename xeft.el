8850838345decbc1bd22112ebf63d6107ca85b9c
Yuan Fu <casouri@gmail.com>
2021-09-13 10:42:22 -0700

    Fix "create note" prompt
    * xeft.el (xeft-refresh): Don't show "create note" prompt if search
    phrase is empty.

--- xeft.el
+++ xeft.el
@@ -494,7 +494,8 @@ non-nil, display all results."
                           file search-phrase))
                      ;; NOTE: this string is referred in
                      ;; ‘xeft-create-note’.
-                     (insert "Press RET to create a new note"))
+                     (unless phrase-empty
+                       (insert "Press RET to create a new note")))
                    ;; Insert clipped notice.
                    (when list-clipped
                      (insert
