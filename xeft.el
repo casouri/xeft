45d975d54d097cdebef7a02b12eea7e190b53808
Yuan Fu <casouri@gmail.com>
2023-01-02 19:25:21 -0800

    Prepare for ELPA
    * xeft.el: Add copyright notice, require subr-x.

--- xeft.el
+++ xeft.el
@@ -1,7 +1,28 @@
 ;;; xeft.el --- Deft feat. Xapian      -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2019-2021 Free Software Foundation, Inc.
+
 ;; Author: Yuan Fu <casouri@gmail.com>
-;; Version: 3.0
+;; Maintainer: Yuan Fu <casouri@gmail.com>
+;; URL: https://git.sr.ht/~casouri/xeft
+;; Version: 3.1
+;; Keywords: Applications, Note, Searching
+;; Package-Requires: ((emacs "26.0"))
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; This file is NOT part of GNU Emacs
 
@@ -57,6 +78,7 @@
 ;;; Code:
 
 (require 'cl-lib)
+(require 'subr-x) ; ‘string-trim’
 (declare-function xapian-lite-reindex-file nil
                   (path dbpath &optional lang force))
 (declare-function xapian-lite-query-term nil
